ea7541d7143f9dc74eb429b301a7ebc0cab4fa4d
MuhammadRioAprilianto <rma994636@gmail.com>
2025-05-07 15:06:58 +0700

    membuat funtion input

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -4,3 +4,27 @@ using namespace std;
 int element[10]; //membuat array
 int nPanjang; //membuat input panjang data
 int x; //nilai yang dicari
+
+void input()
+{ //prosedur input
+    while (true){
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10 data): ";
+        cin >> nPanjang;
+        if (nPanjang <= 10){
+            break;
+        }
+        else{
+            cout << "\nArray maksimal 10 elemen\n";
+        }
+    }
+    cout << endl;
+    cout << "=====================" << endl;
+    cout << "Masukkan elemen array";
+    cout << "=====================" << endl;
+     for (int i=0; i<nPanjang; i++)
+    {
+        cout << "Data ke-" << (i+1) << " = ";
+        cin >> element[i];
+    }
+}
+
